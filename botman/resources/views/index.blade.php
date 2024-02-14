9f83320b900b998e28d5f09121e4dca2e576106f
LorenzoTucceri <64541311+LorenzoTucceri@users.noreply.github.com>
2024-02-14 17:02:26 +0100

    Revert "Merge branch 'main' of https://github.com/AlessandroPio/Software-Architectures"
    This reverts commit 34a42130ca13dbfd4b87a81c8e20cd4c193ea891, reversing
    changes made to d77b769ee7cf2128d0fef7dc304459f9d3b9e5d3.

--- botman/resources/views/index.blade.php
+++ botman/resources/views/index.blade.php
@@ -11,16 +11,215 @@
 
         <!-- Styles -->
         <style>
-            /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */html{line-height:1.15;-webkit-text-size-adjust:100%}body{margin:0}a{background-color:transparent}[hidden]{display:none}html{font-family:system-ui,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,Noto Sans,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;line-height:1.5}*,:after,:before{box-sizing:border-box;border:0 solid #e2e8f0}a{color:inherit;text-decoration:inherit}svg,video{display:block;vertical-align:middle}video{max-width:100%;height:auto}.bg-white{--bg-opacity:1;background-color:#fff;background-color:rgba(255,255,255,var(--bg-opacity))}.bg-gray-100{--bg-opacity:1;background-color:#f7fafc;background-color:rgba(247,250,252,var(--bg-opacity))}.border-gray-200{--border-opacity:1;border-color:#edf2f7;border-color:rgba(237,242,247,var(--border-opacity))}.border-t{border-top-width:1px}.flex{display:flex}.grid{display:grid}.hidden{display:none}.items-center{align-items:center}.justify-center{justify-content:center}.font-semibold{font-weight:600}.h-5{height:1.25rem}.h-8{height:2rem}.h-16{height:4rem}.text-sm{font-size:.875rem}.text-lg{font-size:1.125rem}.leading-7{line-height:1.75rem}.mx-auto{margin-left:auto;margin-right:auto}.ml-1{margin-left:.25rem}.mt-2{margin-top:.5rem}.mr-2{margin-right:.5rem}.ml-2{margin-left:.5rem}.mt-4{margin-top:1rem}.ml-4{margin-left:1rem}.mt-8{margin-top:2rem}.ml-12{margin-left:3rem}.-mt-px{margin-top:-1px}.max-w-6xl{max-width:72rem}.min-h-screen{min-height:100vh}.overflow-hidden{overflow:hidden}.p-6{padding:1.5rem}.py-4{padding-top:1rem;padding-bottom:1rem}.px-6{padding-left:1.5rem;padding-right:1.5rem}.pt-8{padding-top:2rem}.fixed{position:fixed}.relative{position:relative}.top-0{top:0}.right-0{right:0}.shadow{box-shadow:0 1px 3px 0 rgba(0,0,0,.1),0 1px 2px 0 rgba(0,0,0,.06)}.text-center{text-align:center}.text-gray-200{--text-opacity:1;color:#edf2f7;color:rgba(237,242,247,var(--text-opacity))}.text-gray-300{--text-opacity:1;color:#e2e8f0;color:rgba(226,232,240,var(--text-opacity))}.text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.text-gray-500{--text-opacity:1;color:#a0aec0;color:rgba(160,174,192,var(--text-opacity))}.text-gray-600{--text-opacity:1;color:#718096;color:rgba(113,128,150,var(--text-opacity))}.text-gray-700{--text-opacity:1;color:#4a5568;color:rgba(74,85,104,var(--text-opacity))}.text-gray-900{--text-opacity:1;color:#1a202c;color:rgba(26,32,44,var(--text-opacity))}.underline{text-decoration:underline}.antialiased{-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.w-5{width:1.25rem}.w-8{width:2rem}.w-auto{width:auto}.grid-cols-1{grid-template-columns:repeat(1,minmax(0,1fr))}@media (min-width:640px){.sm\:rounded-lg{border-radius:.5rem}.sm\:block{display:block}.sm\:items-center{align-items:center}.sm\:justify-start{justify-content:flex-start}.sm\:justify-between{justify-content:space-between}.sm\:h-20{height:5rem}.sm\:ml-0{margin-left:0}.sm\:px-6{padding-left:1.5rem;padding-right:1.5rem}.sm\:pt-0{padding-top:0}.sm\:text-left{text-align:left}.sm\:text-right{text-align:right}}@media (min-width:768px){.md\:border-t-0{border-top-width:0}.md\:border-l{border-left-width:1px}.md\:grid-cols-2{grid-template-columns:repeat(2,minmax(0,1fr))}}@media (min-width:1024px){.lg\:px-8{padding-left:2rem;padding-right:2rem}}@media (prefers-color-scheme:dark){.dark\:bg-gray-800{--bg-opacity:1;background-color:#2d3748;background-color:rgba(45,55,72,var(--bg-opacity))}.dark\:bg-gray-900{--bg-opacity:1;background-color:#1a202c;background-color:rgba(26,32,44,var(--bg-opacity))}.dark\:border-gray-700{--border-opacity:1;border-color:#4a5568;border-color:rgba(74,85,104,var(--border-opacity))}.dark\:text-white{--text-opacity:1;color:#fff;color:rgba(255,255,255,var(--text-opacity))}.dark\:text-gray-400{--text-opacity:1;color:#cbd5e0;color:rgba(203,213,224,var(--text-opacity))}.dark\:text-gray-500{--tw-text-opacity:1;color:#6b7280;color:rgba(107,114,128,var(--tw-text-opacity))}}
+
         </style>
 
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
             }
+
+            .menu {
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 200px;
+                height: 100vh;
+                background-color: #0047ab; /* Blu scuro per il menu */
+                color: #fff; /* Colore del testo del menu */
+                padding: 20px;
+                box-sizing: border-box;
+                z-index: 100;
+            }
+
+            .menu ul {
+                list-style: none;
+                padding: 0;
+                margin: 0;
+                max-height: 63%; /* Imposta l'altezza massima del menu delle chat */
+            }
+
+            .menu ul {
+                overflow-y: auto; /* Aggiunge la barra di scorrimento verticale se necessario */
+            }
+
+
+            .menu li {
+                margin-bottom: 10px;
+            }
+
+            .menu a {
+                color: #fff; /* Colore del testo dei link del menu */
+                text-decoration: none;
+                display: block;
+                padding: 5px 10px;
+                border-radius: 3px;
+                transition: background-color 0.3s ease;
+            }
+
+            .menu a:hover {
+                background-color: #0066cc; /* Blu leggermente più chiaro al passaggio del mouse sui link del menu */
+            }
+            .separator {
+                border-top: 1px solid #ccc;
+                margin: 10px 0;
+            }
+            .menu h1 {
+                font-size: 24px;
+                margin-bottom: 10px;
+                margin-top: 0; /* Aggiunto per spostare il titolo più in alto */
+            }
+            .popup-content {
+                background-color: #fff;
+                margin: 10% auto; /* Ridotto margine superiore */
+                padding: 20px;
+                border-radius: 10px;
+                width: 60%;
+                box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            }
+
+            .close {
+                color: #aaa;
+                float: right;
+                font-size: 28px;
+                font-weight: bold;
+                cursor: pointer;
+            }
+
+            .close:hover,
+            .close:focus {
+                color: #000;
+                text-decoration: none;
+            }
+
+            .input-container {
+                margin-bottom: 20px;
+                display: flex;
+                width: 40%;
+            }
+
+            .input-container input {
+                flex: 1;
+                padding: 8px;
+                border: 1px solid #ccc;
+                border-radius: 5px 0 0 5px;
+                width: 65%; /* Ridotto il campo input */
+                margin-right: 5px; /* Aggiunto margine destro */
+            }
+
+            .input-container button, #microservicesTable button {
+                padding: 10px 20px;
+                border: none;
+                border-radius: 5px;
+                background-color: #408591;
+                color: #fff;
+                cursor: pointer;
+                transition: background-color 0.3s ease;
+                margin-left: 5px; /* Aggiunto margin-left */
+            }
+
+            .input-container button:hover, #microservicesTable button:hover {
+                background-color: #00557e;
+            }
+
+
+            #microservicesTable {
+                max-height: 300px; /* Altezza massima */
+                overflow-y: auto; /* Scroll verticale */
+            }
+
+
+            table {
+                width: 100%;
+                border-collapse: collapse;
+            }
+
+            th, td {
+                padding: 10px;
+                text-align: left;
+                border-bottom: 1px solid #ddd;
+            }
+
+            th {
+                background-color: #f2f2f2;
+            }
+
         </style>
     </head>
+    <body>
+    <div class="menu">
+        <h1>MiLA4U</h1>
+        @if(Auth()->user()->role=="Admin")
+        <div class="separator"></div>
+        <ul>
+            <li><a href="#" onclick="openPopup()">Add microservice</a></li>
+        </ul>
+        @endif
+        <div class="separator"></div>
+        <h2>Chat</h2>
+        <ul>
+            @foreach(\App\Models\Chat::all() as $chat)
+                <li><a href="chat?{{$chat->id}}">{{$chat->name}}</a></li>
+            @endforeach
+        </ul>
+        <div class="separator"></div>
+        <ul>
+            <li><a href="{{ route('logout') }}">Logout</a></li></ul>
+        <div class="separator"></div>
+
+
+    </div>
+    <div id="popup" class="popup" style="display: none">
+        <div class="popup-content">
+            <span class="close" onclick="closePopup()">&times;</span>
+            <h2>Add Microservices</h2>
+            <div class="input-container">
+                <form action="{{ route('addMicroservice') }}" method="POST">
+                    @csrf
+                  <td> <input type="text" id="microserviceName" placeholder="Add Microservice" name="name"></td>
+                    <td>
+                        <button type="submit">Add</button>
+                    </td>
+                </form>
+            </div>
+            <table id="microservicesTable">
+                <thead>
+                <tr>
+                    <th>Microservice Name</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach(\App\Models\Microservice::all() as $microservice)
+                    <tr>
+                        <td>{{ $microservice->name }}</td>
+                        <td><button onclick="deleteMicroservice({{ $microservice->id }})">Delete</button></td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+    </body>
     <body class="antialiased">
         @include('bots.chat')
     </body>
+
+<script>
+    // Funzione per aprire il pop-up
+    function openPopup() {
+        document.getElementById('popup').style.display = 'block';
+        document.getElementById('chat').style.display = 'none';
+
+    }
+
+    // Funzione per chiudere il pop-up
+    function closePopup() {
+        document.getElementById('popup').style.display = 'none';
+        document.getElementById('chat').style.display = 'block';
+
+    }
+
+
+</script>
 </html>
